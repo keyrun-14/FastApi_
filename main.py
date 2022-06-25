86bfb5be4c5e9f9edf14eb34db44b0a498eaa152
keyrun-14 <98756285+keyrun-14@users.noreply.github.com>
2022-06-25 18:15:47 +0530

    pushed readme file

--- main.py
+++ main.py
@@ -1,191 +1,211 @@
-
-### importing required modules
-
-from fastapi import Depends, FastAPI,Request
-
+from fastapi import Depends, FastAPI, Request
 from sqlalchemy.orm import Session
-
-#importing from local files
-from schemas import schemas
-from models import models
-from database.db import SessionLocal, engine
+from schemas import schemas 
+from models import models  
+from database.db import SessionLocal, engine  
 from coordinate.coordinate import Coordinates
-
 import mpu
+from datetime import datetime
+import logging
+# Logging is a Python module in the standard library that provides the facility
+#  to work with the framework for releasing log messages from the Python programs.
 
-app = FastAPI()  ## creating fastapi server
+now =datetime.now()
+time = now.strftime("%d_%m_%Y_%H_%M_%S")
+file_name = f'logs/log_{time}.txt'
+logging.basicConfig(filename=file_name, filemode='a',format='%(asctime)s - %(levelname)s - %(message)s')
 
-models.Base.metadata.create_all(engine)   
-###  create_all() function uses the engine object to create all the defined table objects and stores the information in metadata.
+# Creating an object
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+app = FastAPI()  # creating fastapi server
+models.Base.metadata.create_all(engine)
+# create_all() function uses the engine object to create all the defined table objects and stores the information in metadata.
 
-
-##### connecting the database
-def fetching_Db():
+# connecting the database
+def fetching_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-
-### getting all data from database
+# getting all data from database
 @app.get("/getall")
-def get_all( db :Session = Depends(fetching_Db)):
-        details = db.query(models.CityDetail).all()
+def get_all(db: Session = Depends(fetching_db)):
+    logger.info("Requesting all data")
+    details = db.query(models.CityDetail).all()
+    if details:
+        logger.info("Returning all data")
         return details
+    else:
+        logger.info("No data available")
+        return "No data available."
 
-
-###     getting data by id  from database
+# getting data by id  from database
 @app.get("/gettingbyid/{id}")
-def get_by_id( id,db :Session = Depends(fetching_Db)):
-        details = db.query(models.CityDetail).where(models.CityDetail.id==id).first()
+def get_by_id(id, db: Session = Depends(fetching_db)):
+    logger.info(f"Request: id: {id}")
+    details = db.query(models.CityDetail).where(models.CityDetail.id == id).first()
+    if details:
+        logger.info(f"Return: data of id: {id}")
         return details
+    else:
+        logger.info("request id not available")
+        return "request id not available"
 
-######     getting all details which are related to given request in search bar which contain a part of city name in table
+# getting all details which are related to given request in search bar which contain a part of city name in table
 @app.get("/gettingbysearcha part of city name in table/{city}")
-def get_by_search_a_part_of_city_name_table( city,db :Session = Depends(fetching_Db)):
-        details = db.query(models.CityDetail).filter(models.CityDetail.city.like(city+'%')).all()
+def get_by_search_a_part_of_city_name_table(city, db: Session = Depends(fetching_db)):
+    logger.info("request for {city} details")
+    details = db.query(models.CityDetail).filter(models.CityDetail.city.like(city + '%')).all()
+    if details:
+        logger.info("returning for {city} details")
         return details
+    else:
+        logger.info("no such {city} details")
+        return "no such {city} details"
 
-###     posting data into database
+# posting data into database
 @app.post("/postingCoordinates")
-def createCoordinates(coOrd: schemas.CityDetails, db :Session = Depends(fetching_Db)):  
+def createCoordinates(coOrd: schemas.CityDetails, db: Session = Depends(fetching_db)):
+    logger.info(f"POST Request: Coordinates")
     city = coOrd.city
     state = coOrd.state
     pincode = coOrd.pincode
-    address=city,state,pincode
- 
-    locationData = Coordinates(address)  ###calling coodinates function which will get the coordinates from given api
+    address = city, state, pincode
+    location_data = Coordinates(address)  #calling coodinates function which will get the coordinates from given api
 
-    newCordinate = models.CityDetail() ### creating a new row in table  and assigning values to keys respectively
-
+    newCordinate = models.CityDetail()  # creating a new row in table  and assigning values to keys respectively
     newCordinate.city = city
     newCordinate.state = state
     newCordinate.pincode = pincode
-    newCordinate.longitude =  locationData[1]
-    newCordinate.latitude =  locationData[0] 
+    newCordinate.longitude = location_data[1]
+    newCordinate.latitude = location_data[0]
+    try:
+        db.add(newCordinate)  # adding row to table
+    except Exception as e:
+        logger.error(f"DB ERROR: {e}")
+    db.commit()  # confirming our changes to database
+    logger.info(f"POST Request: Success")
+    return newCordinate
 
-    db.add(newCordinate)    ### adding row to table
-    db.commit()    ####  confirming our changes to database 
-    return newCordinate,locationData
- 
- ####     updating data of particular id
+# updating data of particular id
 @app.put("/updatingCity/{id}")
-def updating_city(coOrd: schemas.CityDetails,id,db:Session=Depends(fetching_Db)):
+def updating_city(coOrd: schemas.CityDetails, id, db: Session = Depends(fetching_db)):
+    logger.info(f"update request for {id}")
     city = coOrd.city
     state = coOrd.state
     pincode = coOrd.pincode
-    address=city,state,pincode
- 
-    locationData = Coordinates(address)   ###calling coodinates function which will get the coordinates from given api
+    address = city, state, pincode
 
-    updateCoordinate ={}    ####updating a row in table  and modifying values to keys respectively
+    location_data = Coordinates(address)  #calling coodinates function which will get the coordinates from given api
+
+    updateCoordinate = {}            #updating a row in table  and modifying values to keys respectively
     updateCoordinate["city"] = city
     updateCoordinate["state"] = state
-    updateCoordinate["pincode"]= pincode
-    updateCoordinate["longitude"] =  locationData[1]
-    updateCoordinate["latitude"] = locationData[0] 
-    
+    updateCoordinate["pincode"] = pincode
+    updateCoordinate["longitude"] = location_data[1]
+    updateCoordinate["latitude"] = location_data[0]
 
-    updating=db.query(models.CityDetail).where(models.CityDetail.id==id).update(updateCoordinate)
-    if updating:   ### if any rows contains same id as in params  in table 
-        db.commit()    ####  confirming our changes to database 
-        return updating,updateCoordinate
-    else:     ### if no row found
-        return "no city with this id "+id
+    updating = db.query(models.CityDetail).where(models.CityDetail.id == id).update(updateCoordinate)
+    if updating:          # if any rows contains same id as in params  in table
+        try:                   
+            db.commit()                   #  confirming our changes to database
+            logger.info(f"retuning request for {id}")
+            return updating, updateCoordinate        
+        except Exception as e:
+            logger.error(f"DB ERROR: {e}")
+    else:  # if no row found
+            logger.info("no city with this id " + id)
+            return "no city with this id " + id
 
-
- ####    deleting data of particular id
+# deleting data of particular id
 @app.delete("/deleteingDetails/{id}")
-def deleteCoordinates(id,db:Session=Depends(fetching_Db)):
-    deleting=db.query(models.CityDetail).where(models.CityDetail.id==id).delete()
-    ## deleting if params id is found 
+def delete_coordinates(id, db: Session = Depends(fetching_db)):
+    logger.info(f"delete request for {id}")
+    deleting = db.query(models.CityDetail).where(models.CityDetail.id == id).delete()
+    # deleting if params id is found 
 
-    if deleting:  ### if any rows contains same id as in params  in table 
-        db.commit()   ####  confirming our changes to database 
-        print(deleting)
-        return deleting
-    else:  ### if no row found
-        return "no city with this id "+id
+    if deleting:  # if any rows contains same id as in params  in table 
+        try:
+            db.commit()  #  confirming our changes to database
+            logger.info(f"deleted {id}")
+            return deleting
+        except Exception as e:
+            logger.error(f"DB ERROR: {e}")
+    else:  # if no row found
+        logger.info("no city with this id " + id)
+        return "no city with this id " + id
 
- ####  deleting all data  in table
+
+#  deleting all data  in table
 @app.delete("/deleteing_all_Details")
-def delete_All_Coordinates(db:Session=Depends(fetching_Db)):
+def delete_all_coordinates(db: Session = Depends(fetching_db)):
+    logger.info("delete request for all data")
+    deleting = db.query(models.CityDetail).delete()
 
-    deleting=db.query(models.CityDetail).delete()
+    if deleting:  # if any rows are found in table 
+        try:
+            db.commit()  #  confirming our changes to database
+            logger.info(f"deleted all data")
+            return "deleted all data in table"
+        except Exception as e:
+            logger.error(f"DB ERROR: {e}")
 
-    if deleting:  ### if any rows are found in table 
-        db.commit()   ####  confirming our changes to database 
-        return "deleted all data in table"
-
-    else:   ### if no rows found in table 
+    else:  # if no rows found in table
+        logger.info("table empty ")
         return "table empty "
 
-        
-###  nearest cities of particular city 
+#  nearest cities of particular city 
 @app.get("/nearestCities/{id}")
-def nearestCoordinates(id,db:Session=Depends(fetching_Db)):
-    current=db.query(models.CityDetail).where(models.CityDetail.id==id).first()
-    currrent_lat=current.latitude
-    current_lon=current.longitude
-    getall=db.query(models.CityDetail).all()
-    nearestcities=[]
-    d=[]
-    for each in getall:
-        ######  using mpu which is collection of functions , we are using this to calculate distance between 2 coordinates
-        dist = mpu.haversine_distance((currrent_lat, current_lon), (each.latitude,each.longitude))
-        d.append([(currrent_lat, current_lon), (each.latitude,each.longitude),dist])
-       
-        if dist<100 and each!=current:
-            nearestcities.append(each)
-    return nearestcities
+def nearest_coordinates(id, db: Session = Depends(fetching_db)):
+    logger.info(f"nearestCities request for {id}")
+    current = db.query(models.CityDetail).where(models.CityDetail.id == id).first()
+    currrent_lat = current.latitude
+    current_lon = current.longitude
+    getall = db.query(models.CityDetail).all()
+    if getall:
+        nearestcities = []
+        d = []
+        for each in getall:
+            #  using mpu which is collection of functions , we are using this to calculate distance between 2 coordinates
+            dist = mpu.haversine_distance((currrent_lat, current_lon), (each.latitude, each.longitude))
+            d.append([(currrent_lat, current_lon), (each.latitude, each.longitude), dist])
 
+            if dist < 100 and each != current:
+                nearestcities.append(each)
+        logger.info(f" returning nearestCities for {id}")
+        return nearestcities
+    else:
+        logger.info("table empty ")
+        return "db empty"
 
-##### nearest cities to city which is not in database
+# nearest cities to city which is not in database
 @app.get("/nearestCities_of_city_not_in_database")
-def nearestCities(city,state,pincode ,db:Session=Depends(fetching_Db)):
-    
-    address=city,state,int(pincode)
-    locationData = Coordinates(address)
-    currrent_lat=float(locationData[0])
-    current_lon=float(locationData[1])
-    getall=db.query(models.CityDetail).all()
-    nearestcities=[]
-    d=[]
-    for each in getall:
-        ######  using mpu which is collection of functions , we are using this to calculate distance between 2 coordinates
-        dist = mpu.haversine_distance((currrent_lat, current_lon), (each.latitude,each.longitude))
-        d.append([(currrent_lat, current_lon), (each.latitude,each.longitude),dist])
-        if dist<100:
-            nearestcities.append(each)
-    return nearestcities
-
-
-# from math import sin, cos, sqrt, atan2, radians
-
-# # approximate radius of earth in km
-# R = 6373.0
-
-# # lat1 = radians(52.2296756)
-# # lon1 = radians(21.0122287)
-# # lat2 = radians(52.406374)
-# # lon2 = radians(16.9251681)
-# # Point one
-# lat1 = radians(28.6139)
-# lon1 =  radians(77.2090)
-
-# # Point two
-# lat2 = radians(17.6868)
-# lon2 = radians(83.2185)
-
-# dlon = lon2 - lon1
-# dlat = lat2 - lat1
-
-# a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-# c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-# distance = R * c
-
-# print("Result:", distance)
-# print("Should be:", 278.546, "km")
-##########   THANK YOU  ####################
+def nearest_cities(city, state, pincode, db: Session = Depends(fetching_db)):
+    logger.info(f"request for nearestCities to  {city}, {state}, {pincode}")
+    address = city, state, int(pincode)
+    location_data = Coordinates(address)
+    currrent_lat = float(location_data[0])
+    current_lon = float(location_data[1])
+    getall = db.query(models.CityDetail).all()
+    if getall:
+        nearestcities = []
+        d = []
+        for each in getall:
+            #  using mpu which is collection of functions , we are using this to calculate distance between 2 coordinates
+            dist = mpu.haversine_distance((currrent_lat, current_lon), (each.latitude, each.longitude))
+            d.append([(currrent_lat, current_lon), (each.latitude, each.longitude), dist])
+            if dist < 100:
+                nearestcities.append(each)
+        if nearestcities:
+            logger.info("returning for nearestCities to" + city + state+ pincode)
+            return nearestcities
+        else:
+            logger.info("no such nearest cities")
+            return "no such nearest cities"
+    else:
+        logger.info("table empty ")
+        return "db empty"
+# THANK YOU 
